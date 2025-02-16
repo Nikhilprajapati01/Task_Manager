a3afcf7e17731f488248aa98e47c36f45b335ea6
nikhil_kumar <nkman2002@gmail.com>
2025-02-16 17:03:44 +0530

    make backend

---
+++ Server/src/models/tasks/taskModel.js
@@ -0,0 +1,42 @@
+import mongoose  from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+        title:{
+            type: String,
+            required: true,
+            default: "NO description"
+        },
+        description:{
+            type: String,
+            default: "NO description"
+        },
+        dueDate:{
+            type: String,
+            default: Date.now(),
+        },
+        status:{
+            type: String,
+            emum:["active", "inactive"],
+            default: "active",
+        },
+
+        completed:{
+            type: Boolean,
+            default:false,
+        },
+        priority: {
+            type: String,
+            enum: ["low", "medium", "high"],
+            default: "low",
+          },
+          user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "User",
+            required: true,
+          },
+  
+    } ,{timestamps: true});
+
+    const Taskmodel = mongoose.model("Task", TaskSchema);
+
+    export default Taskmodel
